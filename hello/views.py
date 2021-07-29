764b13ea77a79a7091a5e5d245c89a64eebeb29c
louispuyo <louispuyo@yahoo.fr>
2021-07-29 16:56:09 +0200

    first commit

--- hello/views.py
+++ hello/views.py
@@ -17,3 +17,7 @@ def db(request):
     greetings = Greeting.objects.all()
 
     return render(request, "db.html", {"greetings": greetings})
+
+
+def post(request):
+    return render(request, "post-single.html")
